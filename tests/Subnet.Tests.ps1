2b4a57bd4a8953114c6626cb68783a858d6025e6
Gonzalo Lopez <gmlp.24a@gmail.com>
2019-05-13 17:24:38 -0500

    [FIX]Get-Subnet: wrong Uri [FIX]Remove-Subnet: wrong Uri [FIX]Remove-Subnet: cmdlet add missing ssl validation [REF]Subnet Tests: use a env var and random for New-Subnet test case

--- tests/Subnet.Tests.ps1
+++ tests/Subnet.Tests.ps1
@@ -2,10 +2,11 @@
 . "$PSScriptRoot/HttpPipelineMocking.ps1"
  Describe "New-Subnet" {
      It "Should create a New-Subnet" {
+        $vlanID = Get-Random -Minimum 1 -Maximum 500
         $options = New-DhcpOptionsObject -BootFileName bootifile -DomainName nutanix -TftpServerName 10.250.140.200 -DomainNameServerList "8.8.8.8", "4.2.2.2" -DomainSearchList "terraform.nutanix.com", "terraform.unit.test.com"
         $ipconfig = New-IpConfigObject -DefaultGatewayIp 10.250.141.1 -DhcpOptions $options -AddressIp 10.250.141.254 -PoolList "10.250.141.110 10.250.141.250" -PrefixLength 24 -SubnetIp 10.250.141.0
-        $subnetresources = New-SubnetResourcesObject -IpConfig $ipconfig -VlanId 3896 -SubnetType VLAN
-        $subnetObj = New-SubnetObject -Name CmdletGen -Description "Generated via cmdlet" -Resources $subnetresources -ClusterReferenceUuid 27db5fb2-2aa0-48c4-bd2a-2fccdb8fad36
+        $subnetresources = New-SubnetResourcesObject -IpConfig $ipconfig -VlanId $vlanID -SubnetType VLAN
+        $subnetObj = New-SubnetObject -Name CmdletGen -Description "Generated via cmdlet" -Resources $subnetresources -ClusterReferenceUuid $env:ClusterID 
         $metadata = New-SubnetMetadataObject
 
         $subnet = New-Subnet -Metadata $metadata -Spec $subnetObj -SkipSSL
