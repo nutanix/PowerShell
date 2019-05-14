95cc2d9335253ab2a869d07dee65bbc250eb3a3b
Gonzalo Lopez <gmlp.24a@gmail.com>
2019-05-14 14:27:15 -0500

    [REF]Image-Test split long commands

--- tests/Images.Tests.ps1
+++ tests/Images.Tests.ps1
@@ -2,13 +2,31 @@
 . "$PSScriptRoot/HttpPipelineMocking.ps1"
  Describe "New-Image" {
      It "Should create a New-Image" {
-       $imageUrl = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
-       $resources = New-ImageResourcesObject -SourceUri $imageUrl 
-       $imageObj = New-ImageObject -Name name -Description "Ubuntu mini iso" -Resources $resources
-       $metadata = New-ImageMetadataObject
-       $image = New-Image -Metadata $metadata -Spec $imageObj -SkipSSL
-       $imageUploaded = New-Image -Metadata $metadata -ImageName "Uploaded image" -ImageDescription "Someimage" -SkipSSL
+        $imageUrl = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
 
-       Remove-Image -Uuid $imageUploaded.Metadata.Uuid -SkipSSL
+        $resources = New-ImageResourcesObject `
+            -SourceUri $imageUrl 
+
+        $imageObj = New-ImageObject `
+            -Name name `
+            -Description "Ubuntu mini iso" `
+            -Resources $resources
+
+        $metadata = New-ImageMetadataObject
+
+        $image = New-Image `
+            -Metadata $metadata `
+            -Spec $imageObj `
+            -SkipSSL
+
+        $imageUploaded = New-Image `
+            -Metadata $metadata `
+            -ImageName "Uploaded image" `
+            -ImageDescription "Someimage" `
+            -SkipSSL
+
+        Remove-Image `
+            -Uuid $imageUploaded.Metadata.Uuid `
+            -SkipSSL
      }
  }
