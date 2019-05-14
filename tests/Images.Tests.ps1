dabd6a343aabc108ecf5e06bd63010fe0ba2c5e8
Gonzalo Lopez <gmlp.24a@gmail.com>
2019-05-14 10:19:14 -0500

    [ADD]unittest: Images

---
+++ tests/Images.Tests.ps1
@@ -0,0 +1,14 @@
+# Pull In Mocking Support
+. "$PSScriptRoot/HttpPipelineMocking.ps1"
+ Describe "New-Image" {
+     It "Should create a New-Image" {
+       $imageUrl = "http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/mini.iso"
+       $resources = New-ImageResourcesObject -SourceUri $imageUrl 
+       $imageObj = New-ImageObject -Name name -Description "Ubuntu mini iso" -Resources $resources
+       $metadata = New-ImageMetadataObject
+       $image = New-Image -Metadata $metadata -Spec $imageObj -SkipSSL
+       $imageUploaded = New-Image -Metadata $metadata -ImageName "Uploaded image" -ImageDescription "Someimage" -SkipSSL
+
+       Remove-Image -Uuid $imageUploaded.Metadata.Uuid -SkipSSL
+     }
+ }
