96660f33924a4ad381a0b90f190492862ba35e46
Travis CI <travis@travis-ci.org>
2019-06-19 14:38:16 -0500

    [REF]ci: publish package remove unnessesary files

--- .ci/publish-package.ps1
+++ .ci/publish-package.ps1
@@ -5,8 +5,18 @@ param(
 0
 $ModuleName= "Nutanix"
 
-Import-Module ".\$ModuleName.psd1"
+mkdir  ".\$ModuleName"
+
+Get-ChildItem -Path . | Copy-Item -Destination ".\$ModuleName" -Recurse -Container 
+
+rm -rf ./Nutanix/private/
+rm -rf ./Nutanix/help/   
+rm -rf ./Nutanix/test*   
+
+Import-Module ".\$ModuleName\$ModuleName.psd1"
 
 Get-Module Nutanix
 
-Publish-Module -Name ".\$ModuleName.psd1" -NuGetApiKey $apiKey
+Publish-Module -Name ".\$ModuleName\$ModuleName.psd1" -NuGetApiKey $apiKey
+
+rm -rf ./Nutanix   
